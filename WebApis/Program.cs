e6c396a7431b4b553bf5173889e67e4d5aa854e1
Steven <stevencardona.2021@gmail.com>
2026-09-21 23:53:52 -0500

    update db conection

--- WebApis/Program.cs
+++ WebApis/Program.cs
@@ -53,7 +53,7 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Remoto")));
 
 builder.Services.AddApplicationServices();
 
